a0e49650a61607009b49fa3869a2a379bfe03cec
diallue <diallue@unirioja.es>
2025-05-27 20:34:35 +0200

    arreglos en EKF_GEOS3

--- tests/EKF_GEOS3.cpp
+++ tests/EKF_GEOS3.cpp
@@ -34,36 +34,17 @@ int main() {
     DE430Coeff(2285, 1020);
     AuxParamLoad();
     GEOS3(46);
-	
-	std::cout << "Validating dimensions...\n";
-    if (eopdata.n_row != 13 || eopdata.n_column != 24324) {
-        std::cerr << "Invalid eopdata dimensions: " << eopdata.n_row << " x " << eopdata.n_column << "\n";
-        return 1;
-    }
-    if (Cnm.n_row != 181 || Cnm.n_column != 181 || Snm.n_row != 181 || Snm.n_column != 181) {
-        std::cerr << "Invalid Cnm/Snm dimensions\n";
-        return 1;
-    }
-    if (PC.n_row != 2285 || PC.n_column != 1020) {
-        std::cerr << "Invalid PC dimensions: " << PC.n_row << " x " << PC.n_column << "\n";
-        return 1;
-    }
-    if (obs.n_row != 46 || obs.n_column != 4) {
-        std::cerr << "Invalid obs dimensions: " << obs.n_row << " x " << obs.n_column << "\n";
-        return 1;
-    }
 
-    double sigma_range = 92.5;        
-    double sigma_az = 0.0224 * RAD;    
-    double sigma_el = 0.0139 * RAD;   
+    double sigma_range = 92.5;
+    double sigma_az = 0.0224 * RAD;
+    double sigma_el = 0.0139 * RAD;
 
-    double lat = RAD * 21.5748;       
-    double lon = RAD * (-158.2706);    
-    double alt = 300.20;              
+    double lat = RAD * 21.5748;
+    double lon = RAD * (-158.2706);
+    double alt = 300.20;
 
-	std::cout << "Computing station position...\n";
-    Matrix Rs = Position(lon, lat, alt);
-	std::cout << "Rs dimensions: " << Rs.n_row << " x " << Rs.n_column << "\n";
+    Matrix pos = Position(lon, lat, alt);
+    Matrix Rs = pos;
 
     double Mjd1 = obs(1, 1);
     double Mjd2 = obs(9, 1);
@@ -76,18 +57,15 @@ int main() {
     v2(1, 1) = 3837.62169313926;
     v2(2, 1) = -1382.03320148307;
     v2(3, 1) = -5978.16810402366;
-	std::cout << "r2 dimensions: " << r2.n_row << " x " << r2.n_column << "\n";
-    std::cout << "v2 dimensions: " << v2.n_row << " x " << v2.n_column << "\n";
-    //anglesg(obs(1, 2), obs(9, 2), obs(18, 2), obs(1, 3), obs(9, 3), obs(18, 3), Mjd1, Mjd2, Mjd3, Rs, Rs, Rs, r2, v2);
 
     Matrix Y0_apr(6, 1);
-    for (int i = 1; i <= 3; ++i) {
+    for (int i = 1; i <= 3; i++) {
         Y0_apr(i, 1) = r2(i, 1);
         Y0_apr(i + 3, 1) = v2(i, 1);
     }
-	std::cout << "Y0_apr dimensions: " << Y0_apr.n_row << " x " << Y0_apr.n_column << "\n";
 
     double Mjd0 = Mjday(1995, 1, 29, 2, 38, 0);
+
     double Mjd_UTC = obs(9, 1);
     AuxParam.Mjd_UTC = Mjd_UTC;
     AuxParam.n = 20;
@@ -96,179 +74,142 @@ int main() {
     AuxParam.moon = 1;
     AuxParam.planets = 1;
 
-	std::cout << "Integrating initial orbit...\n";
-    Matrix Y = DEInteg(Accel, 0, -(obs(9, 1) - Mjd0) * 86400.0, 1e-13, 1e-6, 6, Y0_apr);
-	std::cout << "Y dimensions: " << Y.n_row << " x " << Y.n_column << "\n";
+    double n_eqn = 6;
 
-    Matrix P(6, 6);
-    for (int i = 1; i <= 6; ++i) {
-        for (int j = 1; j <= 6; ++j) {
-            P(i, j) = 0.0;
-        }
-    }
-    for (int i = 1; i <= 3; ++i) {
+    Matrix Y = DEInteg(Accel, 0, -(Mjd_UTC - Mjd0) * 86400.0, 1e-13, 1e-6, 6, Y0_apr);
+
+    Matrix P = zeros(6, 6);
+    for (int i = 1; i <= 3; i++) {
         P(i, i) = 1e8;
     }
-    for (int i = 4; i <= 6; ++i) {
+    for (int i = 4; i <= 6; i++) {
         P(i, i) = 1e3;
     }
-	std::cout << "P dimensions: " << P.n_row << " x " << P.n_column << "\n";
 
     Matrix LT = LTC(lon, lat);
+
     Matrix yPhi(42, 1);
     Matrix Phi(6, 6);
-    Matrix Qdt(6, 6);
+
     double t = 0;
-	std::cout << "LT dimensions: " << LT.n_row << " x " << LT.n_column << "\n";
-    std::cout << "yPhi dimensions: " << yPhi.n_row << " x " << yPhi.n_column << "\n";
-    std::cout << "Phi dimensions: " << Phi.n_row << " x " << Phi.n_column << "\n";
-    std::cout << "Qdt dimensions: " << Qdt.n_row << " x " << Qdt.n_column << "\n";
 
-    for (int i = 1; i <= 46; ++i) {
-		std::cout << "\nProcessing measurement " << i << "...\n";
-		double t_old = t;
-		Matrix Y_old = Y;
+    for (int i = 1; i <= 46; i++) {
+        double t_old = t;
+        Matrix Y_old = Y;
 
-		Mjd_UTC = obs(i, 1);
-		t = (Mjd_UTC - Mjd0) * 86400.0;
-		std::cout << "Mjd_UTC: " << Mjd_UTC << ", t: " << t << "\n";
+        Mjd_UTC = obs(i, 1);
+        t = (Mjd_UTC - Mjd0) * 86400.0;
 
-		auto iers_result = IERS(eopdata, Mjd_UTC, 'l');
-		double x_pole = std::get<0>(iers_result), y_pole = std::get<1>(iers_result);
-		double UT1_UTC = std::get<2>(iers_result), LOD = std::get<3>(iers_result);
-		double dpsi = std::get<4>(iers_result), deps = std::get<5>(iers_result);
-		double dx_pole = std::get<6>(iers_result), dy_pole = std::get<7>(iers_result);
-		double TAI_UTC = std::get<8>(iers_result);
+        double x_pole, y_pole, UT1_UTC, LOD, dpsi, deps, dx_pole, dy_pole, TAI_UTC;
+        std::tie(x_pole, y_pole, UT1_UTC, LOD, dpsi, deps, dx_pole, dy_pole, TAI_UTC) = IERS(eopdata, Mjd_UTC, 'l');
 
-		auto time_result = timediff(UT1_UTC, TAI_UTC);
-		double UT1_TAI = std::get<0>(time_result), UTC_GPS = std::get<1>(time_result);
-		double UT1_GPS = std::get<2>(time_result), TT_UTC = std::get<3>(time_result);
-		double GPS_UTC = std::get<4>(time_result);
+        double UT1_TAI, UTC_GPS, UT1_GPS, TT_UTC, GPS_UTC;
+        std::tie(UT1_TAI, UTC_GPS, UT1_GPS, TT_UTC, GPS_UTC) = timediff(UT1_UTC, TAI_UTC);
 
-		double Mjd_TT = Mjd_UTC + TT_UTC / 86400.0;
-		double Mjd_UT1 = Mjd_TT + (UT1_UTC - TT_UTC) / 86400.0;
-		AuxParam.Mjd_UTC = Mjd_UTC;
-		AuxParam.Mjd_TT = Mjd_TT;
+        double Mjd_TT = Mjd_UTC + TT_UTC / 86400.0;
+        double Mjd_UT1 = Mjd_TT + (UT1_UTC - TT_UTC) / 86400.0;
+        AuxParam.Mjd_UTC = Mjd_UTC;
+        AuxParam.Mjd_TT = Mjd_TT;
 
-		for (int ii = 1; ii <= 6; ++ii) {
-			yPhi(ii, 1) = Y_old(ii, 1);
-			for (int j = 1; j <= 6; ++j) {
-				yPhi(6 * (j - 1) + ii, 1) = (ii == j) ? 1.0 : 0.0;
-			}
-		}
+        for (int ii = 1; ii <= 6; ii++) {
+            yPhi(ii, 1) = Y_old(ii, 1);
+            for (int j = 1; j <= 6; j++) {
+                yPhi(6 * j + ii, 1) = (ii == j) ? 1.0 : 0.0;
+            }
+        }
 
-		yPhi = DEInteg (VarEqn,0,t-t_old,1e-13,1e-6,42,yPhi);
-		double yPhi_norm = yPhi.norm();
-		if (yPhi_norm > 1e10 || std::isnan(yPhi_norm)) {
-			std::cerr << "Error: yPhi explota en iteración " << i << ", norm: " << yPhi_norm << "\n";
-			return 1;
-		}
+        yPhi = DEInteg(VarEqn, 0, t - t_old, 1e-13, 1e-6, 42, yPhi);
 
-		for (int j = 1; j <= 6; ++j) {
-			for (int k = 1; k <= 6; ++k) {
-				Phi(k, j) = yPhi(6 * (j - 1) + k, 1);
-			}
-		}
+        for (int j = 1; j <= 6; j++) {
+            for (int k = 1; k <= 6; k++) {
+                Phi(k, j) = yPhi(6 * j + k, 1);
+            }
+        }
 
-		Y = DEInteg (Accel,0,t-t_old,1e-13,1e-6,6,Y_old);
-		double Y_norm = Y.norm();
-		if (Y_norm > 1e10 || std::isnan(Y_norm)) {
-			std::cerr << "Error: Y explota en iteración " << i << ", norm: " << Y_norm << "\n";
-			return 1;
-		}
+        Y = DEInteg(Accel, 0, t - t_old, 1e-13, 1e-6, 6, Y_old);
 
-		double theta = gmst(Mjd_UT1);
-		Matrix U = R_z(theta);
-		Matrix r(3, 1);
-		for (int j = 1; j <= 3; ++j) {
-			r(j, 1) = Y(j, 1);
-		}
-		Matrix s = LT * (U * r - Rs);
+        double theta = gmst(Mjd_UT1);
+        Matrix U = R_z(theta);
+        Matrix r(3, 1);
+        for (int j = 1; j <= 3; j++) {
+            r(j, 1) = Y(j, 1);
+        }
 
-		TimeUpdate(P, Phi, Qdt);
+        Matrix Ur = U * r;
 
-		auto azel_result = AzElPa(s);
-		double Azim = std::get<0>(azel_result), Elev = std::get<1>(azel_result);
-		Matrix dAds = std::get<2>(azel_result), dEds = std::get<3>(azel_result);
+        Matrix s = LT * (Ur - Rs);
 
-		Matrix dAdY(1, 6);
-		Matrix temp = dAds * LT * U;
-		for (int j = 1; j <= 3; ++j) {
-			dAdY(1, j) = temp(1, j);
-			dAdY(1, j + 3) = 0.0;
-		}
+        Matrix Qdt = zeros(6, 6);
+        TimeUpdate(P, Phi, Qdt);
 
-		Matrix K, z(1, 1), g(1, 1), s_sigma(1, 1);
-		z(1, 1) = obs(i, 2); g(1, 1) = Azim; s_sigma(1, 1) = sigma_az;
-		auto meas_result = MeasUpdate(Y, z, g, s_sigma, dAdY, P, 6);
-		K = std::get<0>(meas_result); Y = std::get<1>(meas_result); P = std::get<2>(meas_result);
+        double Azim, Elev;
+        Matrix dAds(1, 3), dEds(1, 3);
+        std::tie(Azim, Elev, dAds, dEds) = AzElPa(s);
+        Matrix dAdY(1, 6);
+        Matrix LT_U = LT * U;
+        for (int j = 1; j <= 3; j++) {
+            dAdY(1, j) = dAds(1, j) * LT_U(1, j);
+            dAdY(1, j + 3) = 0.0;
+        }
 
-		for (int j = 1; j <= 3; ++j) {
-			r(j, 1) = Y(j, 1);
-		}
-		s = LT * (U * r - Rs);
-		azel_result = AzElPa(s);
-		Azim = std::get<0>(azel_result); Elev = std::get<1>(azel_result);
-		dAds = std::get<2>(azel_result); dEds = std::get<3>(azel_result);
+        Matrix K, Y_new, P_new;
+        Matrix z(1, 1), g(1, 1), s_sigma(1, 1);
+        z(1, 1) = obs(i, 2);
+        g(1, 1) = Azim;
+        s_sigma(1, 1) = sigma_az;
+        std::tie(K, Y_new, P_new) = MeasUpdate(Y, z, g, s_sigma, dAdY, P, 6);
+        Y = Y_new;
+        P = P_new;
 
-		Matrix dEdY(1, 6);
-		temp = dEds * LT * U;
-		for (int j = 1; j <= 3; ++j) {
-			dEdY(1, j) = temp(1, j);
-			dEdY(1, j + 3) = 0.0;
-		}
+        for (int j = 1; j <= 3; j++) {
+            r(j, 1) = Y(j, 1);
+        }
+        s = LT * (U * r - Rs);
+        std::tie(Azim, Elev, dAds, dEds) = AzElPa(s);
+        Matrix dEdY(1, 6);
+        for (int j = 1; j <= 3; j++) {
+            dEdY(1, j) = dEds(1, j) * LT_U(1, j);
+            dEdY(1, j + 3) = 0.0;
+        }
 
-		z(1, 1) = obs(i, 3); g(1, 1) = Elev; s_sigma(1, 1) = sigma_el;
-		meas_result = MeasUpdate(Y, z, g, s_sigma, dEdY, P, 6);
-		K = std::get<0>(meas_result); Y = std::get<1>(meas_result); P = std::get<2>(meas_result);
+        z(1, 1) = obs(i, 3);
+        g(1, 1) = Elev;
+        s_sigma(1, 1) = sigma_el;
+        std::tie(K, Y_new, P_new) = MeasUpdate(Y, z, g, s_sigma, dEdY, P, 6);
+        Y = Y_new;
+        P = P_new;
 
-		for (int j = 1; j <= 3; ++j) {
-			r(j, 1) = Y(j, 1);
-		}
-		s = LT * (U * r - Rs);
-		double Dist = s.norm();
-		Matrix dDds = transpose(s / Dist);
-		Matrix dDdY(1, 6);
-		temp = dDds * LT * U;
-		for (int j = 1; j <= 3; ++j) {
-			dDdY(1, j) = temp(1, j);
-			dDdY(1, j + 3) = 0.0;
-		}
+        for (int j = 1; j <= 3; j++) {
+            r(j, 1) = Y(j, 1);
+        }
+        s = LT * (U * r - Rs);
+        double Dist = s.norm();
+        Matrix dDds = transpose(s / Dist);
+        Matrix dDdY(1, 6);
+        for (int j = 1; j <= 3; j++) {
+            dDdY(1, j) = dDds(1, j) * LT_U(1, j);
+            dDdY(1, j + 3) = 0.0;
+        }
 
-		z(1, 1) = obs(i, 4); g(1, 1) = Dist; s_sigma(1, 1) = sigma_range;
-		meas_result = MeasUpdate(Y, z, g, s_sigma, dDdY, P, 6);
-		K = std::get<0>(meas_result); Y = std::get<1>(meas_result); P = std::get<2>(meas_result);
-	}
+        z(1, 1) = obs(i, 4);
+        g(1, 1) = Dist;
+        s_sigma(1, 1) = sigma_range;
+        std::tie(K, Y_new, P_new) = MeasUpdate(Y, z, g, s_sigma, dDdY, P, 6);
+        Y = Y_new;
+        P = P_new;
+    }
 
-	std::cout << "Final IERS call...\n";
     double x_pole, y_pole, UT1_UTC, LOD, dpsi, deps, dx_pole, dy_pole, TAI_UTC;
-    auto iers_result = IERS(eopdata, obs(46, 1), 'l');
-    x_pole = std::get<0>(iers_result);
-    y_pole = std::get<1>(iers_result);
-    UT1_UTC = std::get<2>(iers_result);
-    LOD = std::get<3>(iers_result);
-    dpsi = std::get<4>(iers_result);
-    deps = std::get<5>(iers_result);
-    dx_pole = std::get<6>(iers_result);
-    dy_pole = std::get<7>(iers_result);
-    TAI_UTC = std::get<8>(iers_result);
+    std::tie(x_pole, y_pole, UT1_UTC, LOD, dpsi, deps, dx_pole, dy_pole, TAI_UTC) = IERS(eopdata, obs(46, 1), 'l');
 
-	std::cout << "Final timediff call...\n";
     double UT1_TAI, UTC_GPS, UT1_GPS, TT_UTC, GPS_UTC;
-    auto time_result = timediff(UT1_UTC, TAI_UTC);
-    UT1_TAI = std::get<0>(time_result);
-    UTC_GPS = std::get<1>(time_result);
-    UT1_GPS = std::get<2>(time_result);
-    TT_UTC = std::get<3>(time_result);
-    GPS_UTC = std::get<4>(time_result);
+    std::tie(UT1_TAI, UTC_GPS, UT1_GPS, TT_UTC, GPS_UTC) = timediff(UT1_UTC, TAI_UTC);
 
     double Mjd_TT = Mjd_UTC + TT_UTC / 86400.0;
     AuxParam.Mjd_UTC = Mjd_UTC;
     AuxParam.Mjd_TT = Mjd_TT;
 
-	std::cout << "Final integration...\n";
     Matrix Y0 = DEInteg(Accel, 0, -(obs(46, 1) - obs(1, 1)) * 86400.0, 1e-13, 1e-6, 6, Y);
-	std::cout << "Y0 dimensions: " << Y0.n_row << " x " << Y0.n_column << "\n";
 
     Matrix Y_true(6, 1);
     Y_true(1, 1) = 5753.173e3;
@@ -278,11 +219,11 @@ int main() {
     Y_true(5, 1) = -1.924299e3;
     Y_true(6, 1) = -5.728216e3;
 
-    std::cout << "\nError of Position Estimation\n";
+    std::cout << "\nError de Estimación de Posición\n";
     std::cout << "dX" << std::setw(10) << std::fixed << std::setprecision(1) << Y0(1, 1) - Y_true(1, 1) << " [m]\n";
     std::cout << "dY" << std::setw(10) << std::fixed << std::setprecision(1) << Y0(2, 1) - Y_true(2, 1) << " [m]\n";
     std::cout << "dZ" << std::setw(10) << std::fixed << std::setprecision(1) << Y0(3, 1) - Y_true(3, 1) << " [m]\n";
-    std::cout << "\nError of Velocity Estimation\n";
+    std::cout << "\nError de Estimación de Velocidad\n";
     std::cout << "dVx" << std::setw(8) << std::fixed << std::setprecision(1) << Y0(4, 1) - Y_true(4, 1) << " [m/s]\n";
     std::cout << "dVy" << std::setw(8) << std::fixed << std::setprecision(1) << Y0(5, 1) - Y_true(5, 1) << " [m/s]\n";
     std::cout << "dVz" << std::setw(8) << std::fixed << std::setprecision(1) << Y0(6, 1) - Y_true(6, 1) << " [m/s]\n";
